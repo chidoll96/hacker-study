21cc068e4a7a0a13c1b3e47d2caa76f2c564f07b
chi sub <chidoll96@naver.com>
2015-05-26 21:47:33 +0900

    hh

--- HackerGitStudy/CPPTest/Main.cpp
+++ HackerGitStudy/CPPTest/Main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
+#include <string>
+#include <ctime>
+#include <fstream>
 using namespace std;
 
 //	각 문제를 해결했을 때마다, 해당 시점에서 commit 할 것.
 //	commit 메시지는 자유롭게 해도 됩니다.
-
 /*
-int main()
-{
 	int main(){
 		int *arr = new int[100];
 		int temp;
@@ -33,10 +33,11 @@ int main()
 		}
 		cout << endl;
 
-		return 0;
+	return 0;
 	}
 	*/
-/*#include "person.h"
+/*
+#include "person.h"
 #include <string>
 #include <iostream>
 using namespace std;
@@ -73,34 +74,41 @@ int main(){
 	return 0;
 }*/
 
-	//  3번
-	//	반 성적 관리 시스템
-    //  학생(student)에 대한 구조체와 응시과목(item)에 대한 구조체를 만든다.
-    //
-    //  list.txt 파일을 읽어들여 main에서 student* 포인터를 이용하여 인원별로 동적할당
-    //  또한 응시과목별로 subject에 대한 동적할당을 구현한다.
-    //  파일에서 모든 데이터를 읽었다면
-    //  맨 위에는 "총 응시인원: 00명" 을 표시하고,
-    //  학생이름: 이름 / 응시과목수: 0개 / 과목1: 00, 과목2: 00, 과목3: 00 ...
-    //  와 같은 꼴로 출력한다.
-    //
-    //  추가로 현재 데이터에 한명만 직접 콘솔에서 추가하여
-    //  총 응시인원과 새로운 학생에 대한 점수를 출력하는 코드를 만들어보실 것!
-    //
-    //  struct subject {
-    //      string itemName;
-    //      int score;
-    //  };
-    //  struct student {
-    //      string name;		// 학생이름
-    //      int subjectCount;	// 응시과목수
-    //      subject* pItem;		// 과목 목록
-    //  };
+/*
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
 
-
-	
-	//	4번
-	//	교육자에게 하고싶은 말을 적으시오.
-	//	
+struct subject {
+	string itemName;
+	int score;
+};
+struct student {
+	string name;		// 학생이름
+	int subjectCount;	// 응시과목수
+	subject* pitem;		// 과목 목록
+};
+int main(){
+	ifstream fin;
+	fin.open("list.txt");
+	fin.close();
+	student* arr = new student[6];
+	fin.open("list.txt");
+	for (int i = 0; i < 6; i++)
+	{
+		fin >> arr[i].name >> arr[i].subjectCount;
+		cout << "이름 : " << arr[i].name << " 응시과목수 : " << arr[i].subjectCount << ' ';
+		subject* pitem = new subject[arr[i].subjectCount];
+		for (int j = 0; j < arr[i].subjectCount; j++)
+		{
+			fin >> pitem[j].itemName >> pitem[j].score;
+			cout << pitem[j].itemName << ":" << pitem[j].score << ' ';
+		}
+		cout << endl;
+	}
 	return 0;
 }
+*/
+
+// 어렵네요...
